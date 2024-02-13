d992cfb479a221e01d3bd4e861a4c3e42f3628fb
raphael <raphael.soupayavalliama@gmail.com>
2024-02-13 14:28:33 +0400

    test

--- td1.py
+++ td1.py
@@ -9,7 +9,6 @@ initial = np.array([1, 0, 0])
 # Matrice de transition dans 2 jours
 dans_deux_jours = np.dot(transition, transition)
 
-
 # Proba des états dans deux jours avec la formule : initial * dans_deux_jours
 proba_etat_2 = np.dot(initial, dans_deux_jours)
 
@@ -17,15 +16,13 @@ def affiche(n) :
     if (n==1) :
         print("\n La matrice de transition est : \n", transition)
         print("\n Le vecteur initial est : \n", initial)
-        
+
         print("\nLa probabilité qu'il fasse ensolleillé dans deux jours est de : ", proba_etat_2[0])
         print("La probabilité qu'il fasse nuageux dans deux jours est de : {proba_nuageux:.2f}".format(proba_nuageux = proba_etat_2[1]))
         print("La probabilité qu'il fasse pluvieux dans deux jours est de : ", proba_etat_2[2])
     else :
         return 0
     
-
-
 
 # Simulation temporelle
 def simulation(n) :
